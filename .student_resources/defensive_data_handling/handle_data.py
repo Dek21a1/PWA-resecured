549566eeae401b6aa5fc510a0f69746656246683
Deklan L <124547401+Dek21a1@users.noreply.github.com>
2024-11-25 01:53:06 +0000

    huh

--- .student_resources/defensive_data_handling/handle_data.py
+++ .student_resources/defensive_data_handling/handle_data.py
@@ -5,14 +5,14 @@ import bcrypt
 def check_PSWRD(password: str)  -> bytes:
     if not issubclass(type(password), str):
         raise TypeError("expected string")
-    if not (password).isalnum == True:
+    if not (password).isalnum:
         raise ValueError("did not include any alphanumeric characters")
     if len(password) < 9:
         raise ValueError("too short, below 8 characters")
     if len(password) > 12:
         raise ValueError("too many characters, above 12")
-    if not len(re.search(r"[a-z, A-Z]", password)) == 4:
+    if not len(re.findall(r"[a-z]", password) + re.findall(r"[A-Z]", password)) >= 4:
         raise ValueError("incorrect amount of alpha characters")
-    if not len(re.search(r"[0-9]", password)) == 3:
+    if not len(re.findall(r"[0-9]", password)) >= 3:
         raise ValueError("incorrect amount of numeric characters")
     return password.encode()
--- .student_resources/defensive_data_handling/main.py
+++ .student_resources/defensive_data_handling/main.py
@@ -1,4 +1,4 @@
-import data_handler as sanitiser
+import handle_data as sanitiser
 import logging
 
 logger = logging.getLogger(__name__)
@@ -6,11 +6,11 @@ logging.basicConfig(filename='security_log.log', encoding='utf-8', level=logging
 
 if __name__ == '__main__':
     print (f"Does 'password' meet security requirements: {sanitiser.simple_check_password("password")}" )
-    print (f"Make <HTML> web safe: {sanitiser.make_web_safe('<html>')}")
-    print (f"Is 'name@example.com' an email address: {sanitiser.check_email('name@example.com')}")
-    print (f"Is '123!' an name: {sanitiser.validate_name('123!')}")
-    print (f"Is '1234567890' a number: {sanitiser.validate_number('1234567890')}")
-    print ("--PYTHONIC EXCEPTION HANDLING--")
+    #print (f"Make <HTML> web safe: {sanitiser.make_web_safe('<html>')}")
+    #print (f"Is 'name@example.com' an email address: {sanitiser.check_email('name@example.com')}")
+    #print (f"Is '123!' an name: {sanitiser.validate_name('123!')}")
+    #print (f"Is '1234567890' a number: {sanitiser.validate_number('1234567890')}")
+    #print ("--PYTHONIC EXCEPTION HANDLING--")
 
 #################################################################
 # Improved password checking with Pythonic exception handling   #
@@ -22,9 +22,9 @@ if __name__ == '__main__':
 #################################################################
 
     #password = "!1234abcD&"
-    password = 123
+    password = "134AFEHsdfs"
     try:
-        print (f"The password as the byte string : {sanitiser.check_password(password).hex()} is ready to be encryted" )
+        print (f"The password as the byte string : {sanitiser.check_PSWRD(password).hex()} is ready to be encryted" )
     except TypeError:
         logger.error(f"Type errors for password:{password}")
         print("TypeError has been logged")
